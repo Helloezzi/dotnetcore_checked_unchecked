a7735b4c0fb9d86c33e8fa72b659d5ad307288e1
Heewon, Jung <helloezzi@gmail.com>
2020-02-26 01:19:36 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CheckingForOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(int.MaxValue);
+                int temp = int.MaxValue;
+                //Console.WriteLine(checked(++temp));
+                Console.WriteLine(unchecked(++temp));
+            }
+            catch
+            {
+                Console.WriteLine("An exception occurred because of an overflow.");
+            }
+        }
+    }
+}
